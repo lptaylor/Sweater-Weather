16bbc63fe546b1e0a2f7071e640bec76ee090509
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-25 04:31:00 -0700

    modifies test starting data to accurately reflect dark sky json format

--- spec/models/hourly_weather_spec.rb
+++ spec/models/hourly_weather_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'hourly_weather' do
   before(:each) do
-    @hourly_weather_data = :data [
+    @hourly_weather_data = {:hourly => {data: [
             {
                 :time => 1551088800,
                 :icon => "partly-cloudy-night",
@@ -47,19 +47,19 @@ describe 'hourly_weather' do
                 :time => 1551096000,
                 :icon => "partly-cloudy-night",
                 :temperature => 0.82,
-            }]
-  @h_weather = HourlyWeather.new(@hourly_weather_data)
+            }]}}
+  @h_weather = HourlyWeather.make_hourly_array(@hourly_weather_data)
 
   end
   it 'exists' do
-    expect(@h_weather[0]).to be_a(HourlyWeather)
+    expect(@h_weather[0]).to be_a(Weather)
   end
   it 'is an array size eight' do
     expect(@h_weather.length).to eq(8)
   end
   it 'each object has attributes' do
     expect(@h_weather[0].time).to eq(1551088800)
-    expect(@h_weather[0].temperature).to eq("partly-cloudy-night")
-    expect(@h_weather[0].icon).to eq(51.67)
+    expect(@h_weather[0].icon).to eq("partly-cloudy-night")
+    expect(@h_weather[0].temperature).to eq(51.67)
   end
 end
