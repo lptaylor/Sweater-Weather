7640ff20cf3318fa9235bffe137d2a693c245410
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-25 03:27:51 -0700

    adds vcr and test data

--- spec/services/forcast_service_spec.rb
+++ spec/services/forcast_service_spec.rb
@@ -9,7 +9,14 @@ describe 'forcast service' do
     expect(service).to be_a(ForcastService)
   end
 
-  it 'gives back a response' do
+  it 'gives back a response', :vcr do
+    lat = '39.9205411'
+    lon = '-105.0866504'
+    service = ForcastService.new(lat,lon)
+    response = service.parse_response
     
+    expect(response).to have_key[:currently]
+    expect(response).to have_key[:hourly]
+    expect(response).to have_key[:daily]
   end
 end
