818214a5a016a1216b3688320d0acdacaa6ef782
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-25 04:33:57 -0700

    adds tests for days model and forcast model

---
+++ spec/models/days_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'days' do
+  it 'exists' do
+
+  end
+  it 'has attributes' do
+
+  end
+end
---
+++ spec/models/forcast_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe 'forcast' do
+  
+end
