0567395cf78347cd0bf7702afcd0385673136a86
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-25 09:23:40 -0700

    adds to service test

--- spec/services/giphy_service_spec.rb
+++ spec/services/giphy_service_spec.rb
@@ -4,14 +4,16 @@ describe 'giphy service' do
   before(:each) do
     weather_summary = 'sunny'
     @service = GiphyService.new(weather_summary)
-    @response = @service.parse_response
   end
   it 'exists' do
     expect(@service).to be_a(GiphuyService)
   end
   it 'has response body with keys', :vcr do
-    expect(@response).to have_key(:currently)
-    expect(@response).to have_key(:hourly)
-    expect(@response).to have_key(:daily)
+
+    response = @service.parse_response
+
+    expect(response).to have_key(:currently)
+    expect(response).to have_key(:hourly)
+    expect(response).to have_key(:daily)
   end
 end
