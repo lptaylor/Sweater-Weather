d32bd41174e9f128af17a6655e12dc32fd82ac7a
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-25 11:25:19 -0700

    key for params changed will have to fix later

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    forecast = Forecast.new(params[:location])
+    forecast = Forecast.new(params['forecast'])
     render json: ForecastSerializer.new(forecast)
   end
 end
