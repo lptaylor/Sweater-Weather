08ac804653a3dbb85ab09beaecdad6bf99d13232
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-25 02:43:40 -0700

    adds vcr to test

--- spec/services/geolocator_service_spec.rb
+++ spec/services/geolocator_service_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe 'geolocator service' do
   it 'exists' do
     location = 'Broomfield, Colorado'
-    locator = Geolocator.new(location)
+    locator = GeolocatorService.new(location)
 
-    expect(locator).to be_a Geolocator
+    expect(locator).to be_a GeolocatorService
   end
-  it 'finds lat and long' do
+  it 'finds lat and long', :vcr do 
     location = 'Broomfield, Colorado'
-    locator = Geolocator.new(location)
+    locator = GeolocatorService.new(location)
 
     expect(locator.lat).to eq(39.9205411)
     expect(locator.lon).to eq(-105.0866504)
