54f32fc1342b80b2010df5b208ba5be515e7fe95
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-25 04:00:01 -0700

    finishes testing for current_weather object

--- spec/models/current_weather_spec.rb
+++ spec/models/current_weather_spec.rb
@@ -1,23 +1,37 @@
 require 'rails_helper'
 
 describe 'Current Weather' do
+  before(:each) do
+    @temp = 38.15
+    @feels_like_temp = 40.15
+    @humidity = 0.75
+    @uv_index = 2
+    @visibility = 8.2
+    @summary = 'sunny'
+    @icon = 'partly-cloudy-night'
+    @current_weather_data = {:temperature => @temp,
+                            :apparentTemperature => @feels_like_temp,
+                            :humidity => @humidity,
+                            :uvIndex => @uv_index,
+                            :visibility => @visibility,
+                            :summary => @summary,
+                            :icon => @icon}
+  end
   it 'exists' do
-    temp = 38.15
-    feels_like_temp = 40.15
-    humidity = 0.75
-    uv_index = 2
-    visibility = 8.2
-    summary = 'sunny'
-    icon = 'partly-cloudy-night'
-    current_weather_data = {:temperature => temp,
-                            :apparentTemperature => feels_like_temp,
-                            :humidity => humidity,
-                            :uvIndex => uv_index,
-                            :visibility => visibility,
-                            :summary => summary,
-                            :icon => icon}
-    c_weather = CurrentWeather.new(current_weather_data)
+    c_weather = CurrentWeather.new(@current_weather_data)
 
     expect(c_weather).to be_a(CurrentWeather)
+  end
+
+  it 'has attributes' do
+    c_weather = CurrentWeather.new(@current_weather_data)
+
+    expect(c_weather.temperature).to eq(@temp)
+    expect(c_weather.feels_like_temp).to eq(@feels_like_temp)
+    expect(c_weather.humidity).to eq(@humidity)
+    expect(c_weather.uv_index).to eq(@uv_index)
+    expect(c_weather.visibility).to eq(@visibility)
+    expect(c_weather.summary).to eq(@summary)
+    expect(c_weather.icon).to eq(@icon)
   end
 end
