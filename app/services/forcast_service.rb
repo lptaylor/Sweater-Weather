f23fb67e48c01183d459b244cb33cc1bfefeed5d
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-25 03:12:33 -0700

    adds initialize and get weather to forcast services

--- app/services/forcast_service.rb
+++ app/services/forcast_service.rb
@@ -1,3 +1,15 @@
 class ForcastService
 
+  def initialize(lat, lon)
+    @lat = lat
+    @lon = lon
+  end
+
+  def get_weather
+    Faraday.new(:url => "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{@lat},#{@lon}") do |f|
+      f.params[:exclude] = 'minutely,alerts,flags'
+      f.adapter  Faraday.default_adapter  
+    end
+  end
+
 end
