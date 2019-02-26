21fff57e08b91faf7808b7c103ba54de8f2fc53f
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 12:36:45 -0700

    adds deletion of a location testing to the request spec

--- spec/requests/api/v1/favorite_locations_request_spec.rb
+++ spec/requests/api/v1/favorite_locations_request_spec.rb
@@ -70,4 +70,22 @@ describe 'favorite location request' do
     expect(response).to be_successful
     expect(parsed['status']).to eq(401)
   end
+  it 'allows deletion of a location' do
+    post "/api/v1/users?email=lance@gmail.com&password=123abc&password_confirmation=123abc"
+    user = User.first
+    post "/api/v1/favorites?location=Denver,CO&api_key=#{user.api_key}"
+    post "/api/v1/favorites?location=Boulder,CO&api_key=#{user.api_key}"
+    delete "/api/v1/favorites?location=Denver,CO&api_key=#{user.api_key}"
+
+    parsed = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(parsed).to have_key(:data)
+    expect(parsed[:data][0]).to have_key(:id)
+    expect(parsed[:data][0]).to have_key(:type)
+    expect(parsed[:data][0]).to have_key(:attributes)
+    expect(parsed[:data][0][:attributes]).to have_key(:location)
+    expect(parsed[:data][0][:attributes]).to have_key(:current_weather)
+  end
 end
