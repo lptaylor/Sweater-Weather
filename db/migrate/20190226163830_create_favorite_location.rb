aaaf07320769bf1fbd9b42c1fbae5d60c7b64558
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 09:41:24 -0700

    fix migration to add joins table

--- db/migrate/20190226163830_create_favorite_location.rb
+++ db/migrate/20190226163830_create_favorite_location.rb
@@ -2,7 +2,6 @@ class CreateFavoriteLocation < ActiveRecord::Migration[5.2]
   def change
     create_table :favorite_locations do |t|
       t.string :location
-      t.references :user, foreign_key: true
     end
   end
 end
