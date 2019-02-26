4b3f61b8ec5041fcd567699e28f211286ab09f58
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 15:55:29 -0700

    renames controller for proper routing

---
+++ app/controllers/api/v1/gifs_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::GifsController < ApplicationController
+  def index
+    gifs = GiphyDaysWeatherPresenter.new(params['location'])
+    render json: GifsSerializer.new(gifs)
+  end
+end
--- app/controllers/api/v1/images_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::ImagesController < ApplicationController
-  def index
-    five_day_forecast = Forecast.new(params['forecast']).daily_weather
-    gifs = GiphyDaysWeather.new(five_day_forecast)
-    render json: ImagesSerializer.new(gifs)
-  end
-end
