ffdecd088c7f1e54af1280f8269e2a51ae1d25c4
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 14:34:15 -0700

    Adds additional testing for all methods contained in forcast model

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -8,5 +8,29 @@ RSpec.describe Forecast, type: :model do
       expect(forecast.state).to eq('co')
       expect(forecast.date).to eq(Time.now.strftime('%Y-%m-%d'))
     end
+    it "get_lat", :vcr do
+      forecast = Forecast.new("denver, co")
+      expect(forecast.get_lat).to eq(39.7392358)
+    end
+    it "get_lon", :vcr do
+      forecast = Forecast.new("denver, co")
+      expect(forecast.get_lon).to eq(-104.990251)
+    end
+    it "current_weather", :vcr do
+      forecast = Forecast.new("denver, co")
+      expect(forecast.current_weather).to be_a(CurrentWeather)
+    end
+    it "hourly_weather", :vcr do
+      forecast = Forecast.new("denver, co")
+      expect(forecast.hourly_weather).to be_a(Array)
+      expect(forecast.hourly_weather.length).to eq(8)
+      expect(forecast.hourly_weather[0]).to be_a(Weather)
+    end
+    it "daily_weather", :vcr do
+      forecast = Forecast.new("denver, co")
+      expect(forecast.daily_weather).to be_a(Array)
+      expect(forecast.daily_weather.length).to eq(5)
+      expect(forecast.daily_weather[0]).to be_a(WeatherDay)
+    end
   end
 end
