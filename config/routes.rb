3417e31eb90b4490de8707ca69f70269be2411a9
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 14:40:01 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :forecast, only: [:index]
-<<<<<<< HEAD
-      resources :images, only: [:index]
-=======
       resources :users, only: [:create]
+      resources :images, only: [:index]
       resources :sessions, only: [:create]
       resources :favorites, only: [:create, :index]
       delete '/favorites', to: 'favorites#destroy'
->>>>>>> fe69654907250f1ed38bca68ebb434f1a54bddc5
     end
   end
 end
