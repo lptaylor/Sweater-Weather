004c11a0d9a9f6f392237c3c8f0cd9f664f53a60
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 11:54:54 -0700

    adds route to show index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resources :forecast, only: [:index]
       resources :users, only: [:create]
       resources :sessions, only: [:create]
-      resources :favorites, only: [:create]
+      resources :favorites, only: [:create, :index]
     end
   end
 end
