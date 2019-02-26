49f5dd5269ed9bff1cf8a8b0406a836f401933a0
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 08:21:12 -0700

    fixes failing tests

--- spec/services/forecast_service_spec.rb
+++ spec/services/forecast_service_spec.rb
@@ -4,15 +4,15 @@ describe 'forecast service' do
   it 'exists' do
     lat = 39.9205411
     lon = -105.0866504
-    service = forecastService.new(lat,lon)
+    service = ForecastService.new(lat,lon)
 
-    expect(service).to be_a(forecastService)
+    expect(service).to be_a(ForecastService)
   end
 
   it 'gives back a response', :vcr do
     lat = '39.9205411'
     lon = '-105.0866504'
-    service = forecastService.new(lat,lon)
+    service = ForecastService.new(lat,lon)
     response = service.parse_response
 
     expect(response).to have_key(:currently)
