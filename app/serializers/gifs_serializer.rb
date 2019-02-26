6491b5e88e6acb1a33d18589bdd21e15bf244053
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 15:56:55 -0700

    fixes serializer name to spec

--- app/serializers/gifs_serializer.rb
+++ app/serializers/gifs_serializer.rb
@@ -1,9 +1,7 @@
-class ImagesSerializer
+class GifsSerializer
   include FastJsonapi::ObjectSerializer
   set_id :object_id
-
   attribute :images do |object|
     object.make_giphy_days
   end
-
 end
