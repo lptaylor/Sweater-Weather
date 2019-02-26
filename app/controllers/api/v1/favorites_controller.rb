7f98a08efbf9fe40d2b586432f275fadc7e1cd47
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 11:55:43 -0700

    adds controller method to render index if api_key found

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,8 +1,16 @@
 class Api::V1::FavoritesController < ApplicationController
+  def index
+    user = User.find_by(api_key: params['api_key'])
+    if !user.nil?
+      render json: FavoritesSerializer.new(user.favorite_locations)
+    else
+      render json: {status: 401}
+    end
+  end
 
   def create
     user = User.find_by(api_key: params['api_key'])
-      if !user.nil? 
+      if !user.nil?
         user.favorite_locations.create(location_params)
         render json: {status: 201}
       else
