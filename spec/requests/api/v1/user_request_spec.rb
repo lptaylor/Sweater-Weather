836de10098062c7b13fc5f71db08523012ad214e
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 08:29:16 -0700

    adds testing for session to be created from existing user

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
-describe 'user api creation' do
+describe 'user api' do
   it 'can make user' do
     post "/api/v1/users?email=lance@gmail.com&password=123abc&password_confirmation=123abc"
     parsed = JSON.parse(response.body)
-    
+
+    expect(response).to be_successful
+    expect(parsed).to have_key('api_key')
+    expect(parsed).to have_key('status')
+  end
+
+  it 'can allow existing user to login' do
+    post "/api/v1/users?email=lance@gmail.com&password=123abc&password_confirmation=123abc"
+    post "/api/v1/sessions?email=lance@gmail.com&password=123abc"
+
+    parsed = JSON.parse(response.body)
+
     expect(response).to be_successful
     expect(parsed).to have_key('api_key')
     expect(parsed).to have_key('status')
