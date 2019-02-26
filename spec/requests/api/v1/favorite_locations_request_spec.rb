938456c07689fa8f2224f8abe7cbd5b18500f265
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 11:27:10 -0700

    adds testing to list all favorite locations

--- spec/requests/api/v1/favorite_locations_request_spec.rb
+++ spec/requests/api/v1/favorite_locations_request_spec.rb
@@ -18,4 +18,18 @@ descibe 'favorite location request' do
     expect(response).to be_successful
     expect(parsed['status']).to eq(401)
   end
+  it 'lists all favorite locations for a user' do
+    post "/api/v1/users?email=lance@gmail.com&password=123abc&password_confirmation=123abc"
+    user = User.first
+    post "/api/v1/favorites?location=Denver,CO&api_key=#{user.api_key}"
+    post "/api/v1/favorites?api_key=#{user.api_key}"
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(parsed['status']).to eq(200)
+    expect(parsed).to have_key[:body]
+    expect(parsed[:body][0]).to have_key[:location]
+    expect(parsed[:body][0]).to have_key[:current_weather]
+  end
 end
