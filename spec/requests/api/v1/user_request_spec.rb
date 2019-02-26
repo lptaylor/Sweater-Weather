5609437bfd8a9dd2c8cf4529b48300b5a265f97b
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 10:42:54 -0700

    adds sad path testing for adding a favorite location

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -35,6 +35,18 @@ describe 'user api' do
     post "/api/v1/users?email=lance@gmail.com&password=123abc&password_confirmation=123abc"
     user = User.first
     post "/api/v1/favorites?location=Denver,CO&api_key=#{user.api_key}"
-    binding.pry
+
+    parsed = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(parsed['status']).to eq(201)
+  end
+  it 'cannot add a favorite location without valid api_key' do
+    post "/api/v1/users?email=lance@gmail.com&password=123abc&password_confirmation=123abc"
+    post "/api/v1/favorites?location=Denver,CO&api_key=taco"
+
+    parsed = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(parsed['status']).to eq(401)
   end
 end
