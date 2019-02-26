b6b50e3d33eaf507b7c9a4e93f4203c1cea6f1b9
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-26 08:39:56 -0700

    fixes bad call to check email value

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    user = User.find(params[:email])
+    user = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
         render json: {api_key: user.api_key, status: 200}
       else
