479bb27ebd1dde0363173169a8ec9f07b4b8a781
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-27 07:34:58 -0700

    adds stubbed data folder for any data that needs to be put in there for model tests

---
+++ spec/stubbed_data/stubs.rb
@@ -0,0 +1,11 @@
+def flickr_stub
+  {:id=>"42383111334",
+ :owner=>"132341834@N05",
+ :secret=>"1cdd356459",
+ :server=>"926",
+ :farm=>1,
+ :title=>"Sunset shot from last week",
+ :ispublic=>1,
+ :isfriend=>0,
+ :isfamily=>0}
+end
