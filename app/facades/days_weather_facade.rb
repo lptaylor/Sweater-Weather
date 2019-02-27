e07db3701ba638c0fe47c55588ed4607e87dc719
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-27 10:36:03 -0700

    moves all caching to the individual facades

--- app/facades/days_weather_facade.rb
+++ app/facades/days_weather_facade.rb
@@ -1,6 +1,25 @@
 class DaysWeatherFacade
   def self.get_daily_weather(lat, lon)
-    data = ForecastService.new(lat, lon).parse_response
-    DaysWeather.make_forecast_array(data)
+    if cache_empty_daily?
+      data = ForecastService.new(lat, lon).parse_response
+      cache_result_daily(:daily, DaysWeather.make_forecast_array(data))
+      DaysWeather.make_forecast_array(data)
+    else
+      read_cache_daily
+    end
   end
+end
+
+private
+
+def cache_result_daily(type, weather_data)
+  Rails.cache.write(type, weather_data , expires_in: 12.hours)
+end
+
+def cache_empty_daily?
+  Rails.cache.read(:daily).nil?
+end
+
+def read_cache_daily
+  Rails.cache.read(:daily)
 end
--- app/facades/hourly_weather_facade.rb
+++ app/facades/hourly_weather_facade.rb
@@ -1,6 +1,24 @@
 class HourlyWeatherFacade
   def self.get_hourly_weather(lat, lon)
-    data = ForecastService.new(lat, lon).parse_response
-    HourlyWeather.make_hourly_array(data)
+    if cache_empty_hourly?
+      data = ForecastService.new(lat, lon).parse_response
+      cache_result_hourly(:hourly, HourlyWeather.make_hourly_array(data))
+      HourlyWeather.make_hourly_array(data)
+    else
+      read_cache_hourly
+    end
   end
+end
+private
+
+def cache_result_hourly(type, weather_data)
+  Rails.cache.write(type, weather_data , expires_in: 30.minutes)
+end
+
+def cache_empty_hourly?
+  Rails.cache.read(:hourly).nil?
+end
+
+def read_cache_hourly
+  Rails.cache.read(:hourly)
 end
