f1e9b825a155325ae4d2610da57a37b60e6ec3de
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-27 07:17:14 -0700

    fully adds background service that can receive a response

---
+++ app/services/background_service.rb
@@ -0,0 +1,32 @@
+class BackgroundService
+  def initialize(lat, lon)
+    @lat = lat
+    @lon = lon
+  end
+
+  def conn
+     Faraday.new('https://api.flickr.com/') do |f|
+       f.adapter Faraday.default_adapter
+     end
+  end
+
+  def get_image
+    conn.get('services/rest/') do |c|
+      c.params['api_key'] = ENV['FLICKR_API_KEY']
+      c.params['method'] = 'flickr.photos.search'
+      c.params['sort'] = 'relevance'
+      c.params['tags'] = 'skyline,parks,landmarks'
+      c.params['lat'] = @lat
+      c.params['lon'] = @lon
+      c.params['format'] = "json"
+      c.params['nojsoncallback'] = 1
+      c.params['safe_search'] = 1
+    end
+  end
+
+  def parse_response
+    JSON.parse(get_image.body, symbolize_names: true)
+  end
+
+
+end
