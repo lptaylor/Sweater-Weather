f07e52c7108b1d609939b99daaa34137b8601729
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-27 07:35:14 -0700

    adds to testing

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe 'background image request' do
-  it 'returns an image based on locaion' do
+  it 'returns an image based on locaion', :vcr do
     get '/api/v1/backgrounds?location=denver,co'
 
     parsed = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
+    expect(parsed).to have_key(:id)
   end
 end
