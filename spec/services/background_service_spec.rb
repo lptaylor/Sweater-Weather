5a1ff8cdf9fcc31b873468c4b5ed52d008a7812f
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-27 07:16:54 -0700

    adds key check for a response

--- spec/services/background_service_spec.rb
+++ spec/services/background_service_spec.rb
@@ -10,6 +10,20 @@ describe 'background service' do
   end
 
   it 'returns a response', :vcr do
+    lat = 39.9205411
+    lon = -105.0866504
+    service = BackgroundService.new(lat,lon)
+    response = service.parse_response
 
+    expect(response).to have_key(:photos)
+    expect(response).to have_key(:stat)
+    expect(response[:photos][:photo][0]).to have_key(:id)
+    expect(response[:photos][:photo][0]).to have_key(:owner)
+    expect(response[:photos][:photo][0]).to have_key(:secret)
+    expect(response[:photos][:photo][0]).to have_key(:server)
+    expect(response[:photos][:photo][0]).to have_key(:farm)
+    expect(response[:photos][:photo][0]).to have_key(:ispublic)
+    expect(response[:photos][:photo][0]).to have_key(:isfriend)
+    expect(response[:photos][:photo][0]).to have_key(:isfamily)
   end
 end
