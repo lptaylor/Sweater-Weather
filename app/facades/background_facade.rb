689e57bfd5a17bfc292c6d503252f0b140940762
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-27 07:47:44 -0700

    removes clas method so it can just be initalized via a location

--- app/facades/background_facade.rb
+++ app/facades/background_facade.rb
@@ -1,6 +1,22 @@
 class BackgroundFacade
-  def self.get_url(lat, lon)
-    data = BackgroundService.new(lat, lon).get_background
+  def initialize(location)
+    @location = location
+    @lat = get_lat
+    @lon = get_lon
+  end
+
+  def get_lat
+    service = GeolocatorService.new(@location)
+    service.lat
+  end
+
+  def get_lon
+    service = GeolocatorService.new(@location)
+    service.lon
+  end
+
+  def get_url(lat, lon)
+    data = BackgroundService.new(@lat, @lon).get_background
     Background.new(data)
   end
 end
