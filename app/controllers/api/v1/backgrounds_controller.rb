0509d49a3b896d5217867691125cf46189a27bbf
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-27 09:05:45 -0700

    adds cache of image to expire in one day

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,25 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    background = BackgroundFacade.new(params['location'])
-    render json: BackgroundSerializer.new(background)
+    if cache_empty?
+      background = BackgroundFacade.new(params['location'])
+      render json: BackgroundSerializer.new(background)
+      cache_result(:image, BackgroundSerializer.new(background))
+    else
+      render json: read_cache
+    end
   end
+end
+
+private
+
+def cache_result(image, serializer)
+  Rails.cache.write(image, serializer, expires_in: 86400)
+end
+
+def cache_empty?
+  Rails.cache.read(:image).nil?
+end
+
+def read_cache
+  Rails.cache.read(:image)
 end
